856e360fa7aa51e1cbc845f071ff4aca0b9f8ede
Harrie de Groot <info@harriedegroot.com>
2022-06-09 22:07:34 +0200

    performance

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Engine, MoveDirection, OutMode } from 'tsparticles-engine';
+import { Engine, MoveDirection, OutMode, ParticlesOptions } from 'tsparticles-engine';
 import { loadFull } from "tsparticles";
 import gsap from 'gsap';
 
@@ -11,70 +11,75 @@ import gsap from 'gsap';
 export class BackgroundComponent implements OnInit {
   
   id = 'background_particles';
-  
-  particlesOptions = {
-    background: {
-      color: {
-        value: 'white',
-      },
-    },
-    fpsLimit: 120,
-    particles: {
-      color: {
-        //value: '#000000',
-        value: '#c2185b',
-      },
-      links: {
-        color: '#000000',
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: MoveDirection.none,
-        enable: true,
-        outModes: {
-          default: OutMode.bounce,
-        },
-        random: false,
-        speed: 0.5,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: 'triangle',
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
-    },
-    detectRetina: true,
-  };
+  particlesOptions?: any;
 
   constructor() {}
 
   ngOnInit(): void {
-    gsap.to('.particles', {
-      opacity: 0.4,
+    gsap.to('.particles-container', {
+      opacity: 1,
       duration: 10,
-      delay: 8
+      delay: 8,
+      onStart: () => this.start()
     });
   }
 
-  async particlesInit(engine: Engine): Promise<void> {
+  private start() {
+   this.particlesOptions = {
+      background: {
+        color: {
+          value: 'white',
+        },
+      },
+      fpsLimit: 30,
+      particles: {
+        color: {
+          //value: '#000000',
+          value: '#c2185b',
+        },
+        links: {
+          color: '#000000',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: {
+          enable: false,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.bounce,
+          },
+          random: false,
+          speed: 0.5,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 50,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: 'triangle',
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    };
+  
+  }
+
+  particlesInit = async (engine: Engine): Promise<void> => {
     await loadFull(engine);
   }
 }
