2208cf9d34ebf2b1cf20c6912458a2e43afc7c40
Harrie de Groot <info@harriedegroot.com>
2022-06-26 21:04:33 +0200

    decreased next section navigation speed

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -164,7 +164,7 @@ export class MainComponent implements OnInit {
 
   navigateTo(item: string) {
     gsap.to(window, {
-      duration: 0.2,
+      duration: 0.6,
       scrollTo: '#' + item,
       ease: 'power2',
     });
