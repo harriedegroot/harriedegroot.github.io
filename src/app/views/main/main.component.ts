79023c5fc348091cd525054ddb151d5b10abd346
Harrie de Groot <info@harriedegroot.com>
2022-11-04 09:47:57 +0100

    hide android address-bar test

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -120,6 +120,7 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    window.scrollTo(0,1);
     this.menuOpen = !this.deviceService.isMobile;
     this.initAnimations();
   }
